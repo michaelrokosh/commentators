5fee9a5ce42b36861970e0f280b902bdf38f2b21
Michael Rokosh <mike.rokosh@gmail.com>
2014-01-12 11:28:07 -0500

    Share 2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
 	
   def index
   	@recent_images = Image.paginate(:order => "created_at DESC", :page => params[:page], :per_page => 1)
+  	@image = Image.last
   end
 end

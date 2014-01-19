c08d0d81cb820129899eca59ae389f12dc316b6d
Michael Rokosh <mike.rokosh@gmail.com>
2014-01-19 08:39:57 -0500

    vk auth 2.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,8 @@ class User < ActiveRecord::Base
       User.create!(provider: access_token.provider, url: access_token.info.urls.Vkontakte, avatar_url: access_token.info.image, avatar_big_url: access_token.extra.raw_info.photo_big, username: access_token.info.name, email: access_token.extra.raw_info.screen_name + '@vk.com', nickname: access_token.extra.raw_info.screen_name, password: Devise.friendly_token[0,20]) 
     end
   end
+
+  def voted_for?(votable)
+    votes.where(votable: votable).first.present?
+  end
 end

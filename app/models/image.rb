e0b69f182b73b145f5e316e826ac8aafc9a25b0d
Praxie <mr.praxie@gmail.com>
2014-04-16 19:26:08 +0300

    S3

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,7 +6,7 @@ class Image < ActiveRecord::Base
   has_attached_file :image, styles: {
       thumb: '100x100>',
       square: '200x200#',
-      medium: '500x500>'
+      medium: '600x600>'
     }
 
   # Validate the attached image is image/jpg, image/png, etc

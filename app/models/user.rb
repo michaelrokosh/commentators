d6cdd24037f7398900731b073dac5682632c859e
Praxie <mr.praxie@gmail.com>
2014-04-16 18:37:49 +0300

    S3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,7 @@ class User < ActiveRecord::Base
   has_many :votes, as: :voter
   before_destroy :clear_comments, dependent: :destroy
 
-  has_attached_file :avatar_url, styles: {
-    thumb: '100x100>',
-    square: '200x200#',
-    medium: '300x300>'
-  }
-
   # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :avatar_url, :content_type => /\Aimage\/.*\Z/
 
   def self.find_for_vkontakte_oauth access_token
     if user = User.where(url: access_token.info.urls.Vkontakte).first

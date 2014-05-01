044e6ced3cd3ce45656f5c5d43c1f0433887660c
Praxie <mr.praxie@gmail.com>
2014-05-02 01:30:01 +0300

    yellow lord

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :set_yellow_lord
+
 
   protected
+    def set_yellow_lord
+      @yellow_lord = User.find(14)
+    end
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :username) }
   end

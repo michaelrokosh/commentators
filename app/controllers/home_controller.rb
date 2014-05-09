a11dcc7cdc26f4eddeffd39a8d011e6aeee8aa5c
Praxie <mr.praxie@gmail.com>
2014-05-09 22:23:35 +0300

    rails 4.1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   def index
-  	@recent_images = Image.where(godnota: true).paginate(:order => "created_at DESC", :page => params[:page], :per_page => 3)
+  	@recent_images = Image.where(godnota: true).paginate(order: "created_at DESC", page: params[:page], per_page: 3)
   end
 
   def dniwe
-    @recent_images = Image.where(godnota: false).paginate(:order => "created_at DESC", :page => params[:page], :per_page => 3)
+    @recent_images = Image.where(godnota: false).paginate(order: "created_at DESC", page: params[:page], per_page: 3)
   end
 
   def best
-    @comments = Comment.all.paginate(:order => "rating DESC", :page => params[:page], :per_page => 3)
+    @comments = Comment.all.paginate(order: "rating DESC", page: params[:page], per_page: 3)
   end
 end

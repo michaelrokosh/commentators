5b79eeb080f54fd64efb433a669e91f7ea1b1541
Praxie <mr.praxie@gmail.com>
2014-05-22 20:31:55 +0300

    news

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,7 +38,7 @@ $(function() {
     if ( $( "#vote-block-" + $button.attr('data-comment-id')).hasClass( "make-vote" ) ) {
       $.ajax({
         type: "POST",
-        url: '/images/' + $button.attr('data-image-id') + '/comments/' + $button.attr('data-comment-id') + '/vote_down',
+        url: '/' + $button.attr('data-type') + '/' + $button.attr('data-post-id') + '/comments/' + $button.attr('data-comment-id') + '/vote_down',
         success: function() {
           var rating = parseInt($button.attr('data-comment-rating')) - 1
           $("#vote-block-" + $button.attr('data-comment-id')).removeClass('make-vote').addClass('vote-made').html('<div class="vote-block"><div class="rating">' + rating + '</div><div class="voted"><i class="voted-against fa fa-thumbs-down red"></i></div></div>'); ;
@@ -54,7 +54,7 @@ $(function() {
     if ( $( "#vote-block-" + $button.attr('data-comment-id')).hasClass( "make-vote" ) ) {
       $.ajax({
         type: "POST",
-        url: '/images/' + $button.attr('data-image-id') + '/comments/' + $button.attr('data-comment-id') + '/vote_up',
+        url: '/' + $button.attr('data-type') + '/' + $button.attr('data-post-id') + '/comments/' + $button.attr('data-comment-id') + '/vote_up',
         success: function() {
           var rating = parseInt($button.attr('data-comment-rating')) + 1
           $("#vote-block-" + $button.attr('data-comment-id')).removeClass('make-vote').addClass('vote-made').html('<div class="vote-block"><div class="rating">' + rating + '</div><div class="voted"><i class="voted-against fa fa-thumbs-up green"></i></div></div>'); ; 
@@ -72,7 +72,7 @@ $(function() {
     if ( $( "#vote-block-" + $button.attr('data-comment-id')).hasClass( "make-vote" ) ) {
       $.ajax({
         type: "POST",
-        url: '/images/' + $button.attr('data-image-id') + '/comments/' + $button.attr('data-comment-id') + '/vote_down',
+        url: '/' + $button.attr('data-type') + '/' + $button.attr('data-post-id') + '/comments/' + $button.attr('data-comment-id') + '/vote_down',
         success: function() {
           var rating = parseInt($button.attr('data-comment-rating')) - 1
           $("#vote-block-" + $button.attr('data-comment-id')).removeClass('make-vote').addClass('vote-made').html('<div class="vote-block"><div class="rating">' + rating + '</div></div>'); ;
@@ -88,7 +88,7 @@ $(function() {
     if ( $( "#vote-block-" + $button.attr('data-comment-id')).hasClass( "make-vote" ) ) {
       $.ajax({
         type: "POST",
-        url: '/images/' + $button.attr('data-image-id') + '/comments/' + $button.attr('data-comment-id') + '/vote_up',
+        url: '/' + $button.attr('data-type') + '/' + $button.attr('data-post-id') + '/comments/' + $button.attr('data-comment-id') + '/vote_up',
         success: function() {
           var rating = parseInt($button.attr('data-comment-rating')) + 1
           $("#vote-block-" + $button.attr('data-comment-id')).removeClass('make-vote').addClass('vote-made').html('<div class="vote-block"><div class="rating">' + rating + '</div></div>'); ; 

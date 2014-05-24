4637acb85370607bbfc22b12155616acab3c737e
Praxie <mr.praxie@gmail.com>
2014-05-24 18:21:08 +0300

    partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,14 +3,16 @@ class HomeController < ApplicationController
   def index
     @recent_images = Image.where(godnota: true)
     @recent_news = News.where(godnota: true)
-  	@recent_posts = (@recent_news+@recent_images).sort_by(&:created_at).reverse
+    @recent_facts = Fact.where(godnota: true)
+  	@recent_posts = (@recent_news+@recent_images+@recent_facts).sort_by(&:created_at).reverse
     @recent_posts = @recent_posts.paginate(:page => params[:page], :per_page => 3)
   end
 
   def dniwe
     @recent_images = Image.where(godnota: false)
     @recent_news = News.where(godnota: false)
-    @recent_posts = (@recent_images+@recent_news).sort_by(&:created_at).reverse
+    @recent_facts = Fact.where(godnota: false)
+    @recent_posts = (@recent_images+@recent_news+@recent_facts).sort_by(&:created_at).reverse
     @recent_posts = @recent_posts.paginate(:page => params[:page], :per_page => 3)
   end
 

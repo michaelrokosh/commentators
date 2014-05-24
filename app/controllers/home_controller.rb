abbf22a26e22af3767bfebab28d80aa6678de542
Praxie <mr.praxie@gmail.com>
2014-05-25 00:12:21 +0300

    Stories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     @recent_news = News.where(godnota: true)
     @recent_facts = Fact.where(godnota: true)
     @recent_stories = Story.where(godnota: true)
-    @recent_fictions = Fact.where(godnota: true)
+    @recent_fictions = Fiction.where(godnota: true)
   	@recent_posts = (@recent_news+@recent_images+@recent_facts+@recent_stories+@recent_fictions).sort_by(&:created_at).reverse
     @recent_posts = @recent_posts.paginate(:page => params[:page], :per_page => 3)
   end
@@ -15,7 +15,7 @@ class HomeController < ApplicationController
     @recent_news = News.where(godnota: false)
     @recent_facts = Fact.where(godnota: false)
     @recent_stories = Story.where(godnota: false)
-    @recent_fictions = Fact.where(godnota: false)
+    @recent_fictions = Fiction.where(godnota: false)
     @recent_posts = (@recent_images+@recent_news+@recent_facts+@recent_stories+@recent_fictions).sort_by(&:created_at).reverse
     @recent_posts = @recent_posts.paginate(:page => params[:page], :per_page => 3)
   end
